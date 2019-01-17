dd259bf30e2b64b1ebe4c68e6ac3bd727bca2301
gcmartinelli <gcmartinelli@gmail.com>
2019-01-17 21:02:29 -0200

    Fancy command line commands

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 # checkpass
 
-Short script checking pwnedpasswords.com for a
-chosen password
+Checks pwnedpasswords.com for pwned passwords...
+
+#### Usage
+
+Checks pwnedpasswords.com for pwned passwords
+
+positional arguments:
+  source           filename or password to check
+
+optional arguments:
+  -h, --help       show this help message and exit
+  -f, --filename   use an optional .txt sourcefile. One password per line
+  -v, --verbosity  increase verbosity
+
+##### Default mode 
+
+`python check_passwd.py PASSWORD`
+
+**By default script will only return passwords that have been pwned**
+
+##### File mode ([-f FILENAME])
+
+`python check_passwd.py -f example.txt`
+
+Pass a .txt file with one password per line to check password in bulk.
+
+##### Verbose mode
+
+`python check_passwd.py -v example.txt`
+
+If the flag -v is used, the script will give more details, in terminal, about each password passed. 
+
--- check_passwd.py
+++ check_passwd.py
@@ -2,11 +2,15 @@
 
 '''
 Short script checking pwnedpasswords.com for a
-chosen password
-'''
+chosen password 
 
+http://github.com/gcmartinelli/checkpass
+
+Jan-2019
+'''
 import requests
 import hashlib
+import argparse
 
 def h(string):
 	''' returns sha1 of the password '''
@@ -14,27 +18,103 @@ def h(string):
 	h.update(string.encode())
 	return h.hexdigest()
 
-i = input('Enter password to check...\n')
-hashed = h(i).upper()
+def check_pass(passw):
+	hashed = h(passw).upper()
+	#check API
+	url = 'https://api.pwnedpasswords.com/range/'
+	r = requests.get(url + hashed[:5])
+	n = r.text.find(hashed[5:]) #checks if hash is in the list
 
-url = 'https://api.pwnedpasswords.com/range/'
-r = requests.get(url + hashed[:5])
-n = r.text.find(hashed[5:]) #checks if hash is in the list
+	if n != -1:
+		#creates key value pairs of the returned hashes
+		lines = r.text.split('\n')
+		pairs = {}
+		for line in lines:
+			kv = line.split(':')
+			pairs[kv[0]] = kv[1].replace('\r', '')	
+		return [hashed, pairs[hashed[5:]]]
+	else:
+		return None
 
-if n != -1:
-	print('#'*20)
-	print('FOUND MATCH!')
-	print('#'*20)
+def main():
+	parser = argparse.ArgumentParser(description="Checks pwnedpasswords.com for pwned passwords")
+	parser.add_argument("source", help="filename or password to check")
+	parser.add_argument("-f", "--filename", help="use an optional .txt sourcefile. One password per line", 
+						action="store_true")
+	parser.add_argument("-v", "--verbosity", help="increase verbosity", 
+						action="store_true")
+	args = parser.parse_args()
 
-	#creates key value pairs of the returned hashes
-	lines = r.text.split('\n')
-	pairs = {}
-	for line in lines:
-		kv = line.split(':')
-		pairs[kv[0]] = kv[1].replace('\r', '')
+	if args.verbosity:
+		verbosity = True
+	else:
+		verbosity = False
 	
-	print('Hash: ', hashed)
-	print('Matches: ', pairs[hashed[5:]])
+	source = args.source
 
-else:
-	print('Match not found')
+	if args.filename:
+		with open(source, newline='') as file:
+			for row in file.readlines():
+				passw = row.strip()
+				check = check_pass(passw)
+				if check:
+					if verbosity:
+						print('>>> PASSWORD MATCH! <<<')
+						print('Password: ', passw)
+						print('SHA1 Hash: ', check[0])
+						print('Number of matches: ', check[1])
+						print('-' * 20)
+					else:
+						print(passw)
+				else:
+					if verbosity:
+						print('> Password {} is safu'.format(passw))
+	else:
+		check = check_pass(source)
+		if check:
+			if verbosity:
+				print('>>> PASSWORD MATCH! <<<')
+				print('Password: ', source)
+				print('SHA1 Hash: ', check[0])
+				print('Number of matches: ', check[1])
+				print('-' * 20)
+			else:
+				print(source)
+		else:
+			if verbosity:
+				print('> Password {} is safu'.format(source))
+
+	# # If arguments are passed in command line
+	# if len(sys.argv) > 1:
+	# 	# If 'file' flag is present
+	# 	if if '-f' in sys.argv:
+	# 		if len(sys.argv) != 3:
+	# 			print('A single filename needs to be passed if using the file (-f) flag')
+	# 		else:
+	# 			with open(sys.argv[2], newline='') as file:
+	# 				for row in file.readlines():
+	# 					passw = row.strip()
+	# 					check = check_pass(passw)
+	# 					if check:
+	# 						print('>>> PASSWORD MATCH! <<<')
+	# 						print('Password: ', passw)
+	# 						print('SHA1 Hash: ', check[0])
+	# 						print('Number of matches: ', check[1])
+	# 						print('-' * 20)
+	# 					else:
+	# 						print('> Password {} is safu'.format(passw))
+	# # Fallback if no files are passed in cmd line
+	# else:
+	# 	passw = input('Enter password to check...\n')
+	# 	check = check_pass(passw)
+	# 	if check:
+	# 		print('>>> PASSWORD MATCH! <<<')
+	# 		print('Password: ', passw)
+	# 		print('SHA1 Hash: ', check[0])
+	# 		print('Number of matches: ', check[1])
+	# 		print('-' * 20)
+	# 	else:
+	# 		print('> Password {} is safu'.format(passw))
+
+if __name__ == '__main__':
+	main()
